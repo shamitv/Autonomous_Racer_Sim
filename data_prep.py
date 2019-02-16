12f987eb440b7b5b29a157e9d044a169aa54fb80
Shamit Verma <github@shamit.in>
2019-02-16 19:56:58 +0530

    Data Prep

--- data_prep.py
+++ data_prep.py
@@ -1,5 +1,6 @@
 from os import path
 from os.path import dirname
+import json
 import pandas as pd
 
 def getDataDir():
@@ -16,5 +17,62 @@ def getCaptureDir():
     capture_location = path.normpath(capture_location)
     return capture_location
 
+def getExcelPath():
+    data_dir= getDataDir()
+    excel_path = path.join(data_dir,'set1','data.xlsx')
+    return excel_path
+
+def getImagePath(imgIdx):
+    img_path=path.join(getCaptureDir(),str(imgIdx)+'.png')
+    return img_path
+
+def getJsonPath(imgIdx):
+    img_path=getImagePath(imgIdx)
+    json_path=img_path+'.json'
+    return json_path
+
+def getData(imgIdx):
+    json_path = getJsonPath(imgIdx)
+    with open(json_path) as f:
+        data = json.load(f)
+        return data
+
 
 print(getCaptureDir())
+
+i = 0
+
+data_dict={
+    'imgId':[],
+    'UpArrow':[],
+    'DownArrow':[],
+    'LeftArrow':[],
+    'RightArrow':[],
+    'action':[]
+}
+
+while(path.isfile(getImagePath(i))):
+    data=getData(i)
+    data_dict['imgId'].append(i)
+    data_dict['UpArrow'].append(data['UpArrow'])
+    data_dict['DownArrow'].append(data['DownArrow'])
+    data_dict['LeftArrow'].append(data['LeftArrow'])
+    data_dict['RightArrow'].append(data['RightArrow'])
+    action='DoNothing'
+    if(data['DownArrow']=='down'):
+        action='Break'
+    else:
+        if (data['UpArrow'] == 'down'):
+            action = 'Accelerate'
+        if (data['RightArrow'] == 'down'):
+            action = 'TurnRight'
+        if (data['LeftArrow'] == 'down'):
+            action = 'TurnLeft'
+    data_dict['action'].append(action)
+    i += 1
+
+df = pd.DataFrame(data_dict)
+
+print('Data loaded')
+
+df.to_excel(getExcelPath(),index=False)
